d922091417e83994f881fd7b70b2ae35b10a49ad
dpwong <dpwong@wangdaoledeMacBook-Pro.local>
2017-12-18 14:10:49 +0800

    first commit

---
+++ ButtonDemo/UIButton+DPTitlePostionType.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+DPTitlePostionType.h
+//  ButtonDemo
+//
+//  Created by dpwong on 2017/12/18.
+//  Copyright © 2017年 dpwong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (DPTitlePostionType)
+
+@end
---
+++ ButtonDemo/UIButton+DPTitlePostionType.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+DPTitlePostionType.m
+//  ButtonDemo
+//
+//  Created by dpwong on 2017/12/18.
+//  Copyright © 2017年 dpwong. All rights reserved.
+//
+
+#import "UIButton+DPTitlePostionType.h"
+
+@implementation UIButton (DPTitlePostionType)
+
+@end
